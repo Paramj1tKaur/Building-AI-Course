f26f51bb7230509f79b6e6a54b8a1f1755825f30
Paramj1tKaur <108668314+Paramj1tKaur@users.noreply.github.com>
2023-09-05 01:02:13 +1000

    Delete Chapter 3 -machine learnig directory

--- Chapter 3 -machine learnig/Exercsie15a_nn_vector_distance.py
+++
@@ -1,9 +0,0 @@
-""" Nearest neighbor:  calculate the Euclidean distance between two vectors.
-"""
-import numpy as np
-
-def dist(a, b):
-    sum = 0
-    for ai, bi in zip(a, b):
-        sum = sum + (ai - bi)**2
-    return np.sqrt(sum)
